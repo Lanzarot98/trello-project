db69cf51c6f7ed63172a548edba8080c337f1130
lanzarot <luis-389@hotmail.com>
2023-06-18 21:31:06 -0500

    get information from modals

--- src/app/components/todo-dialog/todo-dialog.component.ts
+++ src/app/components/todo-dialog/todo-dialog.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { DialogRef } from '@angular/cdk/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 // import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faAngleRight, faCheck, faBars, faUser, faTag, faCheckSquare, faClock } from '@fortawesome/free-solid-svg-icons';
+import { ToDo } from 'src/app/models/todo.model';
+
+interface InputData {
+  todo: ToDo;
+}
+interface OutputData {
+  rta: boolean;
+}
 
 @Component({
   selector: 'app-todo-dialog',
@@ -17,15 +25,26 @@ export class TodoDialogComponent implements OnInit {
   faCheckSquare = faCheckSquare;
   faClock = faClock;
 
+  todo: ToDo;
+
   constructor(
-    private dialogRef: DialogRef
-  ) { }
+    private dialogRef: DialogRef<OutputData>,
+    @Inject(DIALOG_DATA) private data: InputData
+  ) {
+    this.todo = data.todo; // se inicializa
+  }
 
   ngOnInit(): void {
   }
 
   close() {
     this.dialogRef.close();
+  }
+
+  closeWithRta(rta: boolean) {
+    this.dialogRef.close({
+      rta
+    });
   }
 
 }
